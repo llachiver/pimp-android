02971d4cbbcfa8c9f7a30e70334ab3a80b0a98e6
romainpc <romainpc.lechat@laposte.net>
2020-02-04 10:09:45 +0100

    oopsi with imports

--- app/src/main/java/fr/ubordeaux/pimp/activity/MainActivity.java
+++ app/src/main/java/fr/ubordeaux/pimp/activity/MainActivity.java
@@ -1,26 +1,15 @@
 package fr.ubordeaux.pimp.activity;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.github.chrisbanes.photoview.PhotoView;
-
-import fr.ubordeaux.pimp.R;
-import fr.ubordeaux.pimp.image.Image;
-import fr.ubordeaux.pimp.io.BitmapIO;
-import fr.ubordeaux.pimp.util.MainSingleton;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import com.github.chrisbanes.photoview.PhotoView;
