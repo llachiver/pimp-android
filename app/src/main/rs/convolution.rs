df953095f7e324e362e373226afd125a36242afd
ricardo0115 <ricardoguevara93@gmail.com>
2020-02-10 21:43:58 +0100

    Some dead comments removed start of separable convolution implementation

--- app/src/main/rs/convolution.rs
+++ app/src/main/rs/convolution.rs
@@ -9,23 +9,18 @@ uint32_t width, height;
 uint32_t kWidth, kHeight;
 static uint32_t kCenterX, kCenterY;
 float kdiv; //For normalize pixel with total value of kernel
-float* kernel;
-float* sobelY;
-float* sobelX;
+const float* kernel;
+const float* sobelY;
+const float* sobelX;
 bool normal;
 
 
 uchar4 RS_KERNEL conv2d(uchar4 in, uint32_t x, uint32_t y)
 {
-    //if (!(x >= (kCenterX-1) && x <= (width - kCenterX) && y >= (kCenterY-1) && y <= (height - kCenterY)))
-    //{
-    //    return rsPackColorTo8888(0.0f,0.0f,0.0f, 1.0f);
-    //}
+
     uint32_t kx, ky;
     float4 temp = 0;
-  //  const uchar4* kin = in - (kCenterX) - (kCenterY) * width; //Bugging shit
-    //rsDebug("kIn", *kin);
-    //You can do better than your code and java if you seek only in the good values range...
+
     uint32_t kIndex = 0;
     for(ky = y - kCenterY; ky <= y + kCenterY ;ky++)
     {
@@ -35,9 +30,7 @@ uchar4 RS_KERNEL conv2d(uchar4 in, uint32_t x, uint32_t y)
            {
                 temp += rsUnpackColor8888( rsGetElementAt_uchar4(pIn, kx, ky)) * kernel[kIndex];
                 kIndex++;
-           }//else
-                //rsDebug("TATA",x,y);
-
+           }
         }
     }
 
@@ -47,6 +40,53 @@ uchar4 RS_KERNEL conv2d(uchar4 in, uint32_t x, uint32_t y)
     temp.a = 1.00f;
     return rsPackColorTo8888(temp);
 }
+/*
+float4 RS_KERNEL convY(uchar4 in, uint32_t x, uint32_t y){
+    uint32_t ky;
+    float4 temp = 0;
+
+    uint32_t kIndex = 0;
+    for(ky = y - kCenterY; ky <= y + kCenterY ;ky++)
+    {
+       if (ky >= (kCenterY) && ky <= (height - kCenterY))
+           {
+           temp += rsUnpackColor8888( rsGetElementAt_uchar4(pIn, kx, ky)) * kernel[kIndex];
+           kIndex++;
+           }
+        }
+    }
+
+    if (normal) temp /= kdiv; //Normalize
+    temp = fabs(temp);
+
+    temp.a = 1.00f;
+    return temp;
+
+}
+
+float4 RS_KERNEL convX(uchar4 in, uint32_t x, uint32_t y){
+    uint32_t kx;
+    float4 temp = 0;
+
+    uint32_t kIndex = 0;
+    for(kx = x - kCenterX; kx <= x + kCenterX ;kx++)
+    {
+        if (kx >= (kCenterX) && kx <= (width - kCenterX))
+           {
+           temp += rsUnpackColor8888( rsGetElementAt_uchar4(pIn, kx, y)) * kernel[kIndex];
+           kIndex++;
+           }
+        }
+    }
+
+    if (normal) temp /= kdiv; //Normalize
+    temp = fabs(temp);
+
+    temp.a = 1.00f;
+    return temp;
+
+}
+*/
 
 
 uchar4 RS_KERNEL conv2dSobel(uchar4 in, uint32_t x, uint32_t y) //Image must be gray!!
@@ -86,7 +126,7 @@ void setup(){
 
 void sobelOperator(rs_allocation inputImage, rs_allocation outputImage){
     rsForEach(grey, inputImage, outputImage); // Turn to gray
-    setup();
+    setup(); //Init kCenters
     rsForEach(conv2dSobel, outputImage, inputImage); //recycle allocation
 
 
