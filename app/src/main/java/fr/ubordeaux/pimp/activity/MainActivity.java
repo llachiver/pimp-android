6e4cdef332cb07481241a5af20277ffd1e3dc1a5
ricardo0115 <ricardoguevara93@gmail.com>
2020-02-18 20:02:21 +0100

    Keep image bug fixed

--- app/src/main/java/fr/ubordeaux/pimp/activity/MainActivity.java
+++ app/src/main/java/fr/ubordeaux/pimp/activity/MainActivity.java
@@ -1,17 +1,18 @@
 package fr.ubordeaux.pimp.activity;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.github.chrisbanes.photoview.PhotoView;
@@ -172,11 +173,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        FragmentManager fm = getFragmentManager();
-        if (fm.getBackStackEntryCount() > 0)
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
             fm.popBackStack();
-        else
-            super.onBackPressed();
+        }
+        else {
+            moveTaskToBack(true);
+        }
     }
 
 
