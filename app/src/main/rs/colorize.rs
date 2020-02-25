b5ca6443a162ed9fdf97699bed2ddb67369160c6
Manuel Guevara <manuel.guevara-garban@etu.u-bordeaux.fr>
2020-02-25 01:05:42 +0100

    Cleaning code and some optimizations

--- app/src/main/rs/colorize.rs
+++ app/src/main/rs/colorize.rs
@@ -1,17 +1,18 @@
-# pragma version (1)
-# pragma rs java_package_name ( fr.ubordeaux.pimp)
+#pragma version (1)
+#pragma rs java_package_name ( fr.ubordeaux.pimp)
+#pragma rs_fp_relaxed
 #include "utils.rs"
 
-double selectedHue;
+int selectedHue;
 bool uniform;
 uchar4 RS_KERNEL colorize ( uchar4 in  ) {
 
-const float4 pixelf = rsUnpackColor8888 ( in ) ;
-const float4 hsv = RGBtoHSV (pixelf);
-if (!uniform )
-hsv.s0 = (int) (((int) hsv.s0) + selectedHue) % 360 ;
-else
-hsv.s0 = (int) selectedHue;
-float4 pixel = HSVtoRGB (hsv);
-return rsPackColorTo8888 ( pixel.s0 , pixel.s1 , pixel.s2 , pixel.s3) ;
+    const float4 pixelf = rsUnpackColor8888 ( in ) ;
+    const float4 hsv = RGBtoHSV (pixelf);
+    if (!uniform )
+        hsv.s0 =(int) (( hsv.s0) + selectedHue) % 360 ;
+    else
+        hsv.s0 = selectedHue;
+    float4 pixel = HSVtoRGB (hsv);
+    return rsPackColorTo8888 ( pixel) ;
 }
--- app/src/main/rs/keepColor.rs
+++ app/src/main/rs/keepColor.rs
@@ -1,16 +1,17 @@
-# pragma version (1)
-# pragma rs java_package_name ( fr.ubordeaux.pimp)
+#pragma version (1)
+#pragma rs java_package_name ( fr.ubordeaux.pimp)
+#pragma rs_fp_relaxed
 #include "utils.rs"
 
-double selectedHue;
-double tolerance;
+int selectedHue;
+int tolerance;
 
 uchar4 RS_KERNEL keepColor (uchar4 in){
     const float4 pixelf = rsUnpackColor8888 ( in ) ;
     const float4 hsv = RGBtoHSV (pixelf);
     if (hsv.s0 >= selectedHue - tolerance && hsv.s0 <= selectedHue + tolerance ){
-    float4 pixel = HSVtoRGB (hsv);
-    return rsPackColorTo8888 ( pixel.s0, pixel.s1, pixel.s2, pixel.s3);
+        float4 pixel = HSVtoRGB (hsv);
+        return rsPackColorTo8888 (pixel);
     }
     else{
         const float gray = dot(pixelf , weight);
