90b7e2c3db774b78e0638d8d91f381da566ed383
picachoc <loic.lachiver@gmail.com>
2020-02-26 22:51:05 +0100

    distorsion now allowed when adjusting contrast

--- app/src/main/rs/dynamicExtension.rs
+++ app/src/main/rs/dynamicExtension.rs
@@ -15,7 +15,7 @@ int factor;
 uchar2 minMaxRGB[NBR_COLOR_CHANS];
 
 //the new min & max RGB values of the bitmap (after the dynamic extension)
-uchar2 newMinMaxRGB[NBR_COLOR_CHANS];
+int2 newMinMaxRGB[NBR_COLOR_CHANS];
 
 
 //LUT for the 3 RGB channels
@@ -24,9 +24,9 @@ static uchar3 lutRGB [LUT_SIZE];
 
 
 void RS_KERNEL computeLutRGB(uchar in, uint32_t x){
-    lutRGB[x].r = newMinMaxRGB[0].x + (newMinMaxRGB[0].y - newMinMaxRGB[0].x) * (x - minMaxRGB[0].x) / (minMaxRGB[0].y - minMaxRGB[0].x);
-    lutRGB[x].g = newMinMaxRGB[1].x + (newMinMaxRGB[1].y - newMinMaxRGB[1].x) * (x - minMaxRGB[1].x) / (minMaxRGB[1].y - minMaxRGB[1].x);
-    lutRGB[x].b = newMinMaxRGB[2].x + (newMinMaxRGB[2].y - newMinMaxRGB[2].x) * (x - minMaxRGB[2].x) / (minMaxRGB[2].y - minMaxRGB[2].x);
+    lutRGB[x].r = truncate(newMinMaxRGB[0].x + (newMinMaxRGB[0].y - newMinMaxRGB[0].x) * (x - minMaxRGB[0].x) / (minMaxRGB[0].y - minMaxRGB[0].x));
+    lutRGB[x].g = truncate(newMinMaxRGB[1].x + (newMinMaxRGB[1].y - newMinMaxRGB[1].x) * (x - minMaxRGB[1].x) / (minMaxRGB[1].y - minMaxRGB[1].x));
+    lutRGB[x].b = truncate(newMinMaxRGB[2].x + (newMinMaxRGB[2].y - newMinMaxRGB[2].x) * (x - minMaxRGB[2].x) / (minMaxRGB[2].y - minMaxRGB[2].x));
 }
 
 
@@ -47,8 +47,8 @@ void computeNewMinMaxRGB(){
     uchar middle;
     for(uchar i = 0; i < NBR_COLOR_CHANS; ++i){
         middle = (minMaxRGB[i].y - minMaxRGB[i].x)/2 + minMaxRGB[i].x;
-        newMinMaxRGB[i].x = middle - factor < 0 ? 0 : middle - factor;
-        newMinMaxRGB[i].y = middle + factor > 255 ? 255 : middle + factor;
+        newMinMaxRGB[i].x = middle - factor;
+        newMinMaxRGB[i].y = middle + factor;
     }
 }
 
